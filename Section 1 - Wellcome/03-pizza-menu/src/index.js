d82d0bd29356ca4b6e758f987b78464174030b66
pkindalov <panayotkindalov@gmail.com>
2024-06-07 10:36:08 +0300

    Creating More Components

--- Section 1 - Wellcome/03-pizza-menu/src/index.js
+++ Section 1 - Wellcome/03-pizza-menu/src/index.js
@@ -51,11 +51,33 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
+  );
+}
+
+function Footer() {
+  // return React.createElement("footer", null, "We're currently open!");
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
   );
 }
 
